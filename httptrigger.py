0ad78a336f1bda87a685f730efcee643913099b9
Quaid Johar <quaid.johar33@gmail.com>
2022-10-26 13:29:56 +0530

    Update httptrigger.py

--- httptrigger.py
+++ httptrigger.py
@@ -6,6 +6,13 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    # Retrieve the html content
     with urllib.request.urlopen('http://python.org/') as response:
         html = response.read()
     return func.HttpResponse(html)
+    
+    # Retrieve response from an api
+    with urllib.request.urlopen('https://catfact.ninja/fact') as response:
+        string = response.read().decode('utf-8')
+        # json_obj = json.loads(string)
+        return func.HttpResponse(string)
