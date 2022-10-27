1e272368fd781d5b5a2d248dcc70e612f492d9a1
Quaid Johar <quaid.johar33@gmail.com>
2022-10-27 13:16:57 +0530

    Update httptrigger.py

--- httptrigger.py
+++ httptrigger.py
@@ -2,7 +2,7 @@ import logging
 import urllib.request
 import azure.functions as func
 
-
+# Get free apis on https://apipheny.io/free-api/
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
